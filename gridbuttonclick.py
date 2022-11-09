651aa05e413f26e96fa8e300813d068aba57e444
Henningson <jann-ole.henningson@fau.de>
2022-11-09 21:05:42 +0100

    Working gridbuttonwindow

--- gridbuttonclick.py
+++ gridbuttonclick.py
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QGraphicsScene, QGraphics
 import skvideo.io
 import sys
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QAction, QGraphicsPolygonItem, QGraphicsEllipseItem
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QAction, QGraphicsPolygonItem, QGraphicsEllipseItem, QDialog
 from PyQt5.QtCore import QSize, pyqtSignal, QPointF
 from PyQt5.QtGui import QIcon, QPen, QBrush, QPolygonF, QColor
 import torch
@@ -21,22 +21,18 @@ from tqdm import tqdm
 import Visualizer
 
 class MainWindow(QMainWindow):  
-    def __init__(self, video_path):
+    def __init__(self):
         QMainWindow.__init__(self)
-        self.setMinimumSize(QSize(800, 600))            
-
-        self.zoom = 1.0
 
         self.setWindowTitle("Structured Light Labelling")
         self.mainwidget = ButtonGrid(18)
+        self.mainwidget.buttonSignal.connect(self.getGridButtonClicked)
         ## Set the central widget of the Window.
         self.setCentralWidget(self.mainwidget)
-    
 
-
-
-
-
+    @QtCore.pyqtSlot(int, int)
+    def getGridButtonClicked(self, x, y):
+        print("Clicked button {} {}".format(x, y))
 
 class ButtonGrid(QWidget):
     buttonSignal = pyqtSignal(int, int)
@@ -45,35 +41,37 @@ class ButtonGrid(QWidget):
         super(ButtonGrid, self).__init__()
 
         self.setLayout(QGridLayout())
-        self.buttons = [ [None]*grid_size for i in range(grid_size)]
+        self.layout().setSpacing(0)
+        self.layout().setContentsMargins(0, 0, 0, 0)
 
         for y in range(grid_size):
             for x in range(grid_size):
                 button = GridButton(x, y)
-                button.clicked.connect(self.clicked_button)
+                button.clickedSignal.connect(self.clicked_button)
                 self.layout().addWidget(button, x, y, 1, 1)
-                self.buttons[x][y] = button
 
+    @QtCore.pyqtSlot(int, int)
     def clicked_button(self, x, y):
-        print(x)
-        print(y)
-
+        self.buttonSignal.emit(x, y)
 
 class GridButton(QPushButton):
-    buttonSignal = pyqtSignal(int, int)
+    clickedSignal = pyqtSignal(int, int)
 
     def __init__(self, x, y, parent=None):
         super(GridButton, self).__init__("")
         self.x = x
         self.y = y
-        self.clicked.emit(x, y)
+        self.clicked.connect(self.on_clicked)
+        self.setContentsMargins(0, 0, 0, 0)
+        self.setFixedSize(QSize(25, 25))
+        self.setStyleSheet("border: 1px solid #333333;")
 
-
+    def on_clicked(self, bool):
+        self.clickedSignal.emit(self.x, self.y)
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
-    shufti = MainWindow("data/Human_P181133_top_Broc5_4001-4200.avi")
+    shufti = MainWindow()
     shufti.show()
     sys.exit(app.exec_())
