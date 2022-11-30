222c1fc0d0061337a5b9c4ef7d0a188fb742b211
Henningson <jann-ole.henningson@fau.de>
2022-11-30 14:57:17 +0100

    More refactoring

--- viewer.py
+++ viewer.py
@@ -4,35 +4,8 @@ from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtGui import QPen, QBrush, QColor
 import numpy as np
 
-class NotImplementedException:
-    pass
-
-gray_color_table = [qRgb(i, i, i) for i in range(256)]
-
-
-def cvImgToQT(im, copy=False):
-    if im is None:
-        return QImage()
-
-    if im.dtype == np.uint8:
-        if len(im.shape) == 2:
-            qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_Indexed8)
-            qim.setColorTable(gray_color_table)
-            return qim.copy() if copy else qim
-
-        elif len(im.shape) == 3:
-            if im.shape[2] == 3:
-                qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_RGB888);
-                return qim.copy() if copy else qim
-            elif im.shape[2] == 4:
-                qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_ARGB32);
-                return qim.copy() if copy else qim
-
-    raise NotImplementedException
-
 
 def cvImgToQT(image):
-    a = image
     height, width, channel = image.shape
     bytesPerLine = 3 * width
     return QImage(np.require(image, np.uint8, 'C'), width, height, bytesPerLine, QImage.Format_RGB888)
